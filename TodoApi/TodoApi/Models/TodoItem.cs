e31f9a856a09115fc7eacc2cdddaeb8ad7bdd880
Jean Visser <VISSIE574@GMAIL.COM>
2024-08-15 12:09:56 +0200

    Giving the classes properties

--- TodoApi/TodoApi/Models/TodoItem.cs
+++ TodoApi/TodoApi/Models/TodoItem.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 namespace TodoApi.Models
 {
-	public class TodoItem
-	{
-		public TodoItem()
-		{
-		}
-	}
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
 }
 
--- TodoApi/TodoApi/Models/User.cs
+++ TodoApi/TodoApi/Models/User.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 namespace TodoApi.Models
 {
-	public class User
-	{
-		public User()
-		{
-		}
-	}
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public ICollection<TodoItem> TodoItems { get; set; }
+    }
 }
 
